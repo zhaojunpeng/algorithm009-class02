755a2307ca7db75695b6da54368f25ca78bae2c7
zhaojunpeng <13811648590@139.com>
2020-05-22 10:51:54 +0800

    create climbing-stairs.cs

---
+++ Week_01/climbing-stairs.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    Dictionary<int, int> dict = new Dictionary<int, int>();
+    public int ClimbStairs(int n) {
+        if(n == 1)
+        {
+            return 1;
+        }
+        if(n == 2)
+        {
+            return 2;
+        }
+        if(dict.ContainsKey(n))
+        {
+            return dict[n]; 
+        }
+        else
+        {
+            int result  = ClimbStairs(n -1) + ClimbStairs(n - 2);
+            dict.Add(n, result);
+            return result;
+        }    
+        
+    }
+}
